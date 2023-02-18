a2fb48f8ab89e46a0b15d1446a96fec9444464aa
nihantelli <nihantelli1@gmail.com>
2023-02-18 12:47:51 +0300

    7.gün güncelleme

--- class.js
+++ class.js
@@ -1,3 +1,4 @@
+"use strict";
 class Vehicle {
   constructor(name, maker, engine) {
     this.name = name;
--- encapsulation.js
+++ encapsulation.js
@@ -1,66 +1,75 @@
 "use strict";
+
 class Person {
-  static ageConst = 60;
+  static #ageConst = 60;
+
   constructor(name, age) {
-    this.name = name; //instance variable
+    this.name = name; // instance variable
     this.age = age;
   }
   calculate(ageConst2) {
-    console.log(`${this.name} ${ageConst2 - this.age}`);
+    console.log(`${this.name}${ageConst2 - thisage}`);
   }
 }
 
 var p1 = new Person("ahmet", 20);
-console.log(p1.calculate());
-
+p1.calculate();
 var p2 = new Person("mehmet", 30);
+
 console.log(Person.ageConst);
 console.log(`${p1.name}${Person.ageConst - p1.age}`);
 console.log(`${p2.name}${Person.ageConst - p2.age}`);
 
+Person.ageConst = 100;
+console.log(`${p1.name}${Person.ageConst - p1.age}`);
+console.log(`${p2.name}${Person.ageConst - p2.age}`);
 
-// class Product {
-//   //dışarıda tanımlıyorsak # işareti koyulur. constructorın içindekiyle bir fakrkı yok. ikisi de private
-//   #stock = 500;
-//   constructor() {
-//     var name = "kalem1";
-//     var price;
-//   }
-//   setStock(stock) {
-//     if (stock > 1000) {
-//       this.stock = stock;
-//       //   veya #stock = stock;
-//     } else {
-//       console.log("stock değeri 1 ile 1000 arasında olmalıdır.");
-//     }
-//   }
-//   getStock() {
-//     return this.#stock;
-//   }
-//   setPrice(price) {
-//     if (price > 1 && price < 100) {
-//       this.price = price;
-//     } else {
-//       console.log("price değeri 1 ile 100 arasındadır.");
-//     }
-//   }
-//   getPrice() {
-//     return this.price;
-//   }
-//   //public method
-//   publicCalculate() {
-//     return this.#Calculate();
-//   }
-//   //private method
-//   #Calculate() {
-//     return 10;
-//   }
-// }
-// var product = new Product();
-// product.setStock(100);
-// console.log(product.getStock());
-// //public, private
-// product.setPrice(200);
-// console.log(product.getPrice());
-// console.log(product.publicCalculate());
-// console.log(product.Calculate());
+class Product {
+  #stock = 500; //private property
+  constructor() {
+    var name;
+    var price;
+  }
+
+  setStock(stock) {
+    if (stock > 1000) {
+      this.#stock = stock;
+    } else throw "stock değeri 1000'den fazla olmalıdır.";
+  }
+
+  getStock() {
+    return this.#stock;
+  }
+
+  setPrice(price) {
+    if (price > 1 && price < 100) {
+      this.price = price;
+    } else {
+      throw "price değeri 1 ile 100 arasında olmalıdır";
+    }
+  }
+
+  getPrice() {
+    return this.price;
+  }
+  //public method
+  publicCalculate() {
+    return this.#Calculate() * 1.18;
+  }
+  //private method
+  #Calculate() {
+    return 10;
+  }
+}
+
+// Access Modifiers
+// public,private,protected  : typescript
+// public,private : javascript
+
+var product = new Product();
+product.setStock(100);
+
+console.log(product.getStock());
+//product.price=100;
+product.setPrice(5);
+console.log(product.getPrice());
