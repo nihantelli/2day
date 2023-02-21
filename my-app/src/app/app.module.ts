50f2eb5038e74dcccd771464e65290859e1a2ff3
nihantelli <111304583+nihantelli@users.noreply.github.com>
2023-02-21 12:06:44 +0300

    12.gün example clear

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,21 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InlineComponent } from './components/inline-component/inline.component';
-import { NormalComponent } from './components/normal/normal.component';
-import { StructuralDirectiveComponent } from './components/structural-directive/structural-directive.component';
-import { ExampleComponent } from './components/example/example.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    InlineComponent,
-    NormalComponent,
-    StructuralDirectiveComponent,
-    ExampleComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
