5f3f131e592a573377b6ef160cd1f17df8bb58e2
nihantelli <111304583+nihantelli@users.noreply.github.com>
2023-02-21 20:43:20 +0300

    12.gün tc gizleme ödevi güncelleme

--- my-app/src/app/pipes/custom-tc.pipe.ts
+++ my-app/src/app/pipes/custom-tc.pipe.ts
@@ -5,9 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CustomTcPipe implements PipeTransform {
   transform(value: unknown, ...args: unknown[]): unknown {
-    let x = '***';
+    let x = '*******';
     let y = value as string;
-    let z = y.slice(3, 11);
-    return x.concat(z);
+    let z = y.slice(0, 4);
+    return z.concat(x);
   }
 }
