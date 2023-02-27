f70a1d5e7806d0de7538a65535241ce9021ed768
nihantelli <111304583+nihantelli@users.noreply.github.com>
2023-02-27 15:51:28 +0300

    16.gün tamamlandı

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TemplateDrivenComponent } from './forms/template-driven/template-driven.component';
+import { TwoWayBindingComponent } from './forms/two-way-binding/two-way-binding.component';
+import { ReactiveFormsComponent } from './forms/reactive-forms/reactive-forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenComponent,
+    TwoWayBindingComponent,
+    ReactiveFormsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
