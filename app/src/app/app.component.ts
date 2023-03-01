af50c797a8f7db05d17088620a83afe9117b32b9
nihantelli <111304583+nihantelli@users.noreply.github.com>
2023-03-01 09:13:46 +0300

    18.gün başlangıç

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular App';
-
-  change() {
-    this.title = 'Angular App 2';
-  }
+  title = 'app';
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShowProductnameComponent } from './components/show-productname/show-productname.component';
 
 @NgModule({
-  declarations: [AppComponent, ShowProductnameComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
