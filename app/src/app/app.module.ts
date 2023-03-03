f15077e90d39a2d1339d92d41055c1fb5163c5c6
nihantelli <111304583+nihantelli@users.noreply.github.com>
2023-03-03 09:18:45 +0300

    20.gün başlangıç

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,20 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PublisherComponent } from './subject/publisher/publisher.component';
-import { Subscriber1Component } from './subject/subscriber1/subscriber1.component';
-import { Subscriber2Component } from './subject/subscriber2/subscriber2.component';
-import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PublisherComponent,
-    Subscriber1Component,
-    Subscriber2Component,
+    AppComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, PagesModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
