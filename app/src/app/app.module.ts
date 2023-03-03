a8c459398300f100cbbcc1844db1fa26b6dd5d52
nihantelli <111304583+nihantelli@users.noreply.github.com>
2023-03-03 13:20:13 +0300

    20.gün 3.çalışma

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
-
 import { BrowserModule } from '@angular/platform-browser';
-import { AdminModule } from './admin/admin.module';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainModule } from './main/main.module';
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AdminModule, MainModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
