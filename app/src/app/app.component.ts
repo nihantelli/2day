95f3db01b06fd1bb626d7fefdd29547bb2b52756
nihantelli <111304583+nihantelli@users.noreply.github.com>
2023-03-06 14:17:00 +0300

    21.gün host-binding başlangıç

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,22 +1,12 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { ProductListWithOkComponent } from './pages/product-list-with-ok/product-list-with-ok.component';
+import {  Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    console.log(this.productListWithOkComponent);
-  }
+export class AppComponent {
+ 
   title = 'app';
 
-
-  @ViewChild(ProductListWithOkComponent) productListWithOkComponent: ProductListWithOkComponent | undefined;
-
-  loadToListWithOkComponent() {
-    console.log("tetiklendi")
-    this.productListWithOkComponent?.load();
-  }
 }
