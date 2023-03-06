f5939508750c648286e376a337c750db61ba4e03
nihantelli <111304583+nihantelli@users.noreply.github.com>
2023-03-06 15:27:37 +0300

    21.gün host-binding tamamlandı

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HighlightDirective } from './directives/highlight.directive';
+import { CounterDirective } from './directives/counter.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  
-
-  ],
+  declarations: [AppComponent, HighlightDirective, CounterDirective],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
